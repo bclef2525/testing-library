86ec81f87724e6db85423cb13ffaaaf05ecd1ca0
bclef2525 <bclef25@gmail.com>
2022-08-23 16:25:05 +0900

    remove overlaped import statement syntax

--- src/pages/Counter/__test__/Counter.test.tsx
+++ src/pages/Counter/__test__/Counter.test.tsx
@@ -1,4 +1,3 @@
-import {} from "@testing-library/react";
 import Counter from "../Counter";
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
